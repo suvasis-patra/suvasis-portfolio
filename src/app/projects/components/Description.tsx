b59178965dbd4799633f5a293099489310a6b0fd
suvasis <suvasis02@kgpian.iitkgp.ac.in>
2024-09-09 18:58:42 +0530

    rename container file to component

--- src/app/projects/components/Description.tsx
+++ src/app/projects/components/Description.tsx
@@ -3,9 +3,6 @@ import ProjectSectionHeader from "./ProjectSectionHeader";
 export default function Description({ description }: { description: string }) {
   return (
     <div className="mt-8">
-      {/* <h3 className="text-xl md:text-3xl font-semibold font-serif tracking-wider uppercase">
-        Description
-      </h3> */}
       <ProjectSectionHeader heading="description" />
       <p className="text-sm lg:text-base text-white/30 mt-4 md:mt-8">
         {description}
